c8a1aaae0159df7b274578e79bdc1ca368566ae0
mikalauskas <me@tranoia.com>
2025-02-20 14:31:23 +0500

    задача 3.1.2

--- src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package ru.kata.spring.boot_security.demo.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.kata.spring.boot_security.demo.model.User;
 
@@ -7,6 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    @EntityGraph(attributePaths = "roles")
     Optional<User> findByUsername(String username);
     void deleteByUsername(String username);
     <T> List<T> getRolesByUsername(String username);
